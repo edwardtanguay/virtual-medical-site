e9e84adcc0c5b766154f8cde44bda1f45ccc0832
Edward Tanguay (windows) <edward@tanguay.info>
2025-02-12 10:41:13 +0100

    changed name

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { flashcardRouter } from "./routers/flashcardRouter";
-import cors from 'cors';
+import cors from "cors";
 
 export const app = express();
 app.use(cors());
@@ -13,10 +13,10 @@ app.get("/", (_req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Info Site</title>
+        <title>Virtual Medical Site</title>
     </head>
     <body>
-        <h1>Info Site</h1>
+        <h1>Virtual Medical Site</h1>
         <p>Version: <strong>1.1</strong></p>
     </body>
     </html>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import { Nav } from "./Nav";
 export const Header = () => {
 	return (
 		<>
-			<h1 className="text-3xl mb-3 text-slate-800">Info Site</h1>
-			<Nav/>
+			<h1 className="text-3xl mb-3 text-slate-800">Virtual Medical Site</h1>
+			<Nav />
 		</>
 	);
 };
